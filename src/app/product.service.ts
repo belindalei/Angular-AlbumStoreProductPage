550bc4af91691623398d5c9a7d82aae8aaf41fe1
Belinda Lei <belinda.lei95@gmail.com>
2020-04-02 09:21:39 -0500

    switched to using Http Module rather than Http Client Module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Http, Response } from '@angular/http'
 import 'rxjs/add/operator/map';
 
 
 @Injectable()
 export class ProductService {
 
-  _albumUrl = "../assets/album.json"
+  private _albumUrl = "../assets/album.json"
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: Http) { }
 
   getAlbum(id: number){
-    return this._http.get(this._albumUrl)
+    return this._http.get(this._albumUrl).map(response => response.json())
   }
 
 }
